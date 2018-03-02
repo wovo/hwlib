df97b42acc7574f504ec669fc2f646231d14eb20
wovo <wouter@voti.nl>
2018-03-02 12:41:58 +0100

    update

--- library/hwlib.hpp
+++ library/hwlib.hpp
@@ -13,39 +13,39 @@
 #ifndef HWLIB_H
 #define HWLIB_H
 
-#define HWCPP_QUOTE( FILE ) #FILE
-#define HWCPP_INCLUDE( FILE ) HWCPP_QUOTE( library/FILE )
+#define HWLIB_QUOTE( FILE ) #FILE
+#define HWLIB_INCLUDE( FILE ) HWLIB_QUOTE( FILE )
 
 #ifdef BMPTK_TARGET_arduino_due
-   HWCPP_INCLUDE( hwlib-arduino-due.hpp )
+   #include HWLIB_INCLUDE( hwlib-arduino-due.hpp )
 #endif
 
 #ifdef BMPTK_TARGET_db103
-   HWCPP_INCLUDE( hwlib-db103.hpp )
+   #include HWLIB_INCLUDE( hwlib-db103.hpp )
 #endif
 
 #ifdef BMPTK_TARGET_arduino_uno
-   HWCPP_INCLUDE( hwlib-arduino-uno.hpp )
+   #include HWLIB_INCLUDE( hwlib-arduino-uno.hpp )
 #endif
 
 #ifdef BMPTK_TARGET_stm32f103c8
-   HWCPP_INCLUDE( hwlib-stm32f103c8.hpp )
+   #include HWLIB_INCLUDE( hwlib-stm32f103c8.hpp )
 #endif
 
 #ifdef BMPTK_TARGET_blue_pill
-   HWCPP_INCLUDE( hwlib-blue-pill.hpp )
+   #include HWLIB_INCLUDE( hwlib-blue-pill.hpp )
 #endif
 
 #ifdef BMPTK_TARGET_native
-   HWCPP_INCLUDE( hwlib-native.hpp )
+   HWLIB_INCLUDE( hwlib-native.hpp )
 #endif
 
 #ifdef BMPTK_TARGET_pyd
-   HWCPP_INCLUDE( hwlib-native.hpp )
+   HWLIB_INCLUDE( hwlib-native.hpp )
 #endif
 
 #ifndef BMPTK_TARGET
-   HWCPP_INCLUDE( hwlib-all.hpp )
+   HWLIB_INCLUDE( hwlib-all.hpp )
 #endif
 
 #endif // HWLIB_ALL_H
