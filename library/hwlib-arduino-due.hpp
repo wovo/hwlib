f3b0a2112d0b0a1fa225e1e851ef9c6fe9c3cbdf
itzandroidtab <koendegraaff@itzandroidtab.nl>
2018-09-06 18:56:10 +0200

    added hardware spi support

--- library/hwlib-arduino-due.hpp
+++ library/hwlib-arduino-due.hpp
@@ -21,6 +21,16 @@
 #include "sam.h"
 #include HWLIB_INCLUDE( hwlib-arduino-due-system-sam3xa.inc )
 
+// Spi Modes
+/** SPI_MODE0 */
+#define SPI_MODE0 0x02
+/** SPI_MODE1 */
+#define SPI_MODE1 0x00
+/** SPI_MODE2 */
+#define SPI_MODE2 0x03
+/** SPI_MODE3 */
+#define SPI_MODE3 0x01
+
 /// \brief
 /// hwlib implementation for the Arduino Due
 /// \details
@@ -666,6 +676,96 @@ public:
    }
 };
 #endif
+
+class hwspi : public hwlib::spi_bus {
+    private:
+        uint8_t chipselect:2;
+
+        constexpr uint32_t SPI_PCS(uint32_t npcs) {
+            return (~(1 << npcs) & 0xf) << 16;
+        }
+
+        void spi_write(uint_fast16_t wData){
+            while ( (SPI0->SPI_SR & SPI_SR_TXEMPTY) == 0 ) ;
+            SPI0->SPI_TDR = wData | SPI_PCS( chipselect ) ;
+            while ( (SPI0->SPI_SR & SPI_SR_TDRE) == 0 ) ;  
+        }
+
+        uint_fast16_t spi_read(){
+            while ( (SPI0->SPI_SR & SPI_SR_RDRF) == 0 ) ;
+            return SPI0->SPI_RDR & 0xFFFF ;            
+        }
+
+        void pin_Configure(const uint32_t dwMask){
+            uint32_t dwSR;
+
+            /* Disable interrupts on the pin(s) */
+            PIOA->PIO_IDR = dwMask;
+            dwSR = PIOA->PIO_ABSR;
+            PIOA->PIO_ABSR &= (~dwMask & dwSR);
+            PIOA->PIO_PDR = dwMask;
+
+            /* Disable interrupts on the pin(s) */
+            PIOA->PIO_IDR = dwMask;
+            // disable pull ups            
+            PIOA->PIO_PUDR = dwMask;
+        }
+
+    public:
+        hwspi(uint8_t chipselect = 0, uint8_t spimode = SPI_MODE0, uint8_t spi_divider = 21, bool loopback = 0):
+            chipselect(chipselect)
+        {
+            // disable write protection spi
+            REG_SPI0_WPMR=0x53504900;
+
+            // disable spi
+            SPI0->SPI_CR = SPI_CR_SPIDIS;
+
+            switch(chipselect){
+                case 1:
+                    // CS1 is pin 4 on the due
+                    pin_Configure(PIO_PA29A_SPI0_NPCS1);
+                    break;
+                case 2:
+                    // not available on arduino due pcb
+                    pin_Configure(PIO_PA30A_SPI0_NPCS2);
+                    break;
+                case 3:
+                    // not available on arduino due pcb
+                    pin_Configure(PIO_PA31A_SPI0_NPCS3);
+                    break;
+                case default:
+                    // CS0 is pin 10 on the due.
+                    pin_Configure(PIO_PA28A_SPI0_NPCS0);
+                    break;                    
+            }
+
+            pin_Configure(PIO_PA25A_SPI0_MISO);
+            pin_Configure(PIO_PA26A_SPI0_MOSI);
+            pin_Configure(PIO_PA27A_SPI0_SPCK);
+
+            // enable pheripheral clock
+            PMC->PMC_PCER0 = 1 << ID_SPI0;
+
+            // Execute a software reset of the SPI twice 
+            SPI0->SPI_CR = SPI_CR_SWRST;
+            SPI0->SPI_CR = SPI_CR_SWRST;        
+
+            // Set SPI configuration parameters.
+            SPI0->SPI_MR = SPI_MR_MSTR | SPI_MR_PS | SPI_MR_MODFDIS | loopback << 7; //SPI_MR_LLB
+            SPI0->SPI_CSR[chipselect] = (spimode & 0x03) | SPI_CSR_SCBR(spi_divider) | SPI_CSR_DLYBCT(1) ;   
+
+            // Enable SPI
+            SPI0->SPI_CR = SPI_CR_SPIEN;                     
+        }
+
+        write_and_read(hwlib::pin_out, const size_t n, const uint8_t data_out[], uint8_t data_in[]){
+            for(uint_fast32_t i = 0; i < n, i++){
+                spi_write(data_out[i]);
+                data_in[i] = spi_read();
+            }
+        }
+}
 
 /// pin_adc implementation for a ATSAM3X8E
 class pin_adc : public hwlib::adc {
